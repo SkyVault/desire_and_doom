e98ae95abe23bfac2877003464d2e020a79eb46f
skyvaultgames <skyvaultgames@gmail.com>
2018-02-24 14:30:13 -0800

    Began work on the dialog box, but then realized that I need a way to draw primitives

--- Desire_And_Doom/Graphics/Dialog_Box.cs
+++ Desire_And_Doom/Graphics/Dialog_Box.cs
@@ -1,4 +1,5 @@
-﻿using Desire_And_Doom.Utils;
+﻿using Desire_And_Doom.Graphics;
+using Desire_And_Doom.Utils;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -13,7 +14,27 @@ using CColor = System.Drawing.Color;
 namespace Desire_And_Doom
 {
     class Dialog_Box
-    { 
-        
+    {
+        public Dialog CurrentDialog {get; private set;} = null;
+        public bool IsOpen { get => CurrentDialog == null; }
+
+        public bool TryOpen(Dialog dialog) {
+            if (IsOpen == false) {
+                CurrentDialog = dialog;
+                return true;
+            }
+            return false;
+        }
+
+        public void Update(GameTime time) {
+            if (!IsOpen) return;
+
+        }
+
+        public void Render(SpriteBatch batch) {
+            if (!IsOpen) return;
+
+            // Draw the dialog box
+        }
     }
 }
